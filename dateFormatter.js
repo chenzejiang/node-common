8f1c38bb4d7bbc345d3aeb60a34aefaedae7f929
chenzejiang <820289461@qq.com>
2019-07-30 09:14:42 +0800

    增加日期格式化方法

---
+++ dateFormatter.js
@@ -0,0 +1,22 @@
+/**
+ * 调用：dateFormatter('YYYY-MM-DD HH:mm', '1995/02/15 13:55') // 1995-02-15 13:55
+ * @param {*} formatter 格式
+ * @param {*} date 日期
+ */
+const dateFormatter = (formatter, date) => {
+	date = (date ? new Date(date) : new Date)
+	const Y = date.getFullYear() + '',
+          M = date.getMonth() + 1,
+          D = date.getDate(),
+          H = date.getHours(),
+          m = date.getMinutes(),
+          s = date.getSeconds()
+  return formatter.replace(/YYYY|yyyy/g, Y)
+                  .replace(/YY|yy/g, Y.substr(2, 2))
+                  .replace(/MM/g, (M < 10 ? '0' : '') + M)
+                  .replace(/DD/g, (D < 10 ? '0' : '') + D)
+                  .replace(/HH|hh/g, (H < 10 ? '0' : '') + H)
+                  .replace(/mm/g, (m < 10 ? '0' : '') + m)
+                  .replace(/ss/g, (s < 10 ? '0' : '') + s)
+}
+export default dateFormatter;
