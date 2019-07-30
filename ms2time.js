ce1b501cd5c2af5964121b3ea151249315016f69
chenzejiang <820289461@qq.com>
2019-07-30 09:14:33 +0800

    优化

--- ms2time.js
+++ ms2time.js
@@ -1,12 +1,12 @@
 /**
  * ms转换可视化阅读时间
- * @param {_time} number
+ * @param {time} 时间戳
  * @returns {string) 2分44秒888毫秒
  */
-const ms2time = (_time = 1000) => {
-    let ms = Number(_time.toString().substr(-3));
-    let seconds = parseInt((_time / 1000) % 60);
-    let min = parseInt(_time / 1000 / 60);
+const ms2time = (time = 1000) => {
+    let ms = Number(time.toString().substr(-3));
+    let seconds = parseInt((time / 1000) % 60);
+    let min = parseInt(time / 1000 / 60);
     return `${min}分${seconds}秒${ms}毫秒`;
 };
 export default ms2time;
