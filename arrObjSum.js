d21318387009ebb6018b6153d68646aa62dea256
chenzejiang <820289461@qq.com>
2020-10-16 10:03:26 +0800

    update

--- arrObjSum.js
+++ arrObjSum.js
@@ -3,6 +3,6 @@
  * @param {Object} arrObj 数组对象
  * @param {String} key 数组对应的 key 值
  */
-export const arrObjSum = (obj, key) => {
-  return arrObj.reduce((prev, cur) => prev + cur.key, 0)
+export const arrObjSum = (arrObj, key) => {
+  return arrObj.reduce((prev, cur) => prev + cur[key], 0)
 }
